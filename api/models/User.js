4b778c1b799813436287370b41684d2d248d64f1
nguyendangbanghn93 <55304669+nguyendangbanghn93@users.noreply.github.com>
2021-07-08 17:17:46 +0700

    up

--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema(
   {
@@ -8,18 +11,31 @@ const UserSchema = new mongoose.Schema(
       min: 3,
       max: 20,
       unique: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       max: 50,
       unique: true,
+      lowercase: true,
+      validate: value => {
+        if (!validator.isEmail(value)) {
+          throw new Error({ error: 'Invalid Email address' })
+        }
+      },
     },
     password: {
       type: String,
       required: true,
       min: 6,
     },
+    tokens: [{
+      token: {
+        type: String,
+        required: true
+      }
+    }],
     profilePicture: {
       type: String,
       default: "",
@@ -59,5 +75,36 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+UserSchema.pre('save', async function (next) {
+  // Hash the password before saving the user model
+  const user = this
+  if (user.isModified('password')) {
+      user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
+})
+UserSchema.methods.generateAuthToken = async function() {
+  // Generate an auth token for the user
+  const user = this
+  const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
+  user.tokens = user.tokens.concat({token})
+  await user.save()
+  return token
+}
+UserSchema.statics.findByCredentials = async (email, password) => {
+  // Search for a user by email and password.
+  console.log("__________2",email);
+  const user = await User.findOne({ email} )
+  console.log("__________3",user);
 
-module.exports = mongoose.model("User", UserSchema);
+  if (!user) {
+      throw new Error({ error: 'Invalid login credentials' })
+  }
+  const isPasswordMatch = await bcrypt.compare(password, user.password)
+  if (!isPasswordMatch) {
+      throw new Error({ error: 'Invalid login credentials' })
+  }
+  return user
+}
+const User = mongoose.model('User', UserSchema)
+module.exports = User;
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,46 +1,33 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
-
+const router = require('express').Router()
+const { models } = require('mongoose')
+const User = require('../models/User')
 //REGISTER
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
+  // Create a new user
   try {
-    //generate new password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-    //create new user
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-    });
-
-    //save user and respond
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json(err)
+    const user = new User(req.body)
+    await user.save()
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
+  } catch (error) {
+    res.status(400).send(error)
   }
-});
-
+})
 //LOGIN
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
+  //Login a registered user
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const { email, password } = req.body
+    console.log("__________1", email);
+    const user = await User.findByCredentials(email, password)
+    console.log("__________4", user);
     if (!user) {
-      return res.status(404).json("user not found");
+      return res.status(401).send({ error: 'Login failed! Check authentication credentials' })
     }
-
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    if (!validPassword) {
-      return res.status(400).json("wrong password")
-    }
-
-    return res.status(200).json(user)
-  } catch (err) {
-    return res.status(500).json(err)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+  } catch (error) {
+    res.status(400).send(error)
   }
-});
-
+})
 module.exports = router;
